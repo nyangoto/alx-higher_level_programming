728f5a391c6b8428a6610f4e34febb954bb5ac34
nyangoto <patricknyangoto@hotmail.com>
2021-04-28 17:53:49 +0000

    Spaces instead of tab MF!

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-	print("{:d} is positive".format(number))
+    print("{:d} is positive".format(number))
 elif number == 0:
-	print("{:d} is zero".format(number))
+    print("{:d} is zero".format(number))
 else:
-	print("{:d} is negative".format(number))
+    print("{:d} is negative".format(number))

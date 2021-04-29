84aa11f4cf16b899e9d1dc10b908564bb9acedc9
nyangoto <patricknyangoto@hotmail.com>
2021-04-29 20:44:40 +0000

    Syntax errror correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,17 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-ops = ['+', '-', '*', '/']
-argc = len(argv)
-
 if __name__ == '__main__':
-    if (argc - 1) < 3:
+    arg_num = len(argv) - 1
+    if arg_num != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    elif argv[2] not in ops:
+    elif argv[2] not in '+-*/':
         print('Unknown operator. Available operators: +, -, *, and /')
         exit(1)
     else:
-        a, b = int(argv[1]), int(argv[3])
+        a = int(argv[1])
+        b = int(argv[3])
         if argv[2] == '+':
             print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
         elif argv[2] == '-':

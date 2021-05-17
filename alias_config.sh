525e86359ca1326b3a63fcfe4b88cd31c409ecde
nyangoto <patricknyangoto@hotmail.com>
2021-05-17 18:55:36 +0000

    Added alias configuration file

---
+++ alias_config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias gs="git status" 
+alias gc="git commit -m" 
+alias ga="git add" 
+alias gp="git push -u origin master"
+alias ex="chmod a+x"
+alias cmp="gcc -Wall -Werror -Wextra -pedantic"
+

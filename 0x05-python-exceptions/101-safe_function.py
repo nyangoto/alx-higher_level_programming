39cc7f8b18df022119fd83666d9cba6d3da6f79b
nyangoto <patricknyangoto@hotmail.com>
2021-05-17 18:43:44 +0000

    Added 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from __future__ import print_function
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None

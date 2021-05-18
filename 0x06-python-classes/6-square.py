84dba4773b6cbea63b5c01bcd80cc0753449ab06
nyangoto <patricknyangoto@hotmail.com>
2021-05-18 20:22:30 +0000

    whitespace correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,7 +63,7 @@ class Square:
         """
         if isinstance(value, tuple) and len(value) == 2:
             if isinstance(value[0], int) and isinstance(value[1], int):
-                if value[0] >= 0 and value [1] >= 0:
+                if value[0] >= 0 and value[1] >= 0:
                     self.__position = value
         else:
             raise TypeError("position must be tuple of 2 positive integers")

49c9c1185be1bf631f166c8b4eff9cbf3f1a837e
nyangoto <patricknyangoto@hotmail.com>
2021-05-18 19:27:13 +0000

    Variable name update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,9 +31,9 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 

f046f654137635f8fe6d7adc602ae6ecb215db6d
nyangoto <patricknyangoto@hotmail.com>
2021-06-01 19:33:45 +0000

    Added 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module for to_json_to-string method.
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns JSON representation of an object.
+    """
+    return (json.dumps(my_obj))

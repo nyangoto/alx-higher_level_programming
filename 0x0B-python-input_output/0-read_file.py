d345f05249ad88477e86ac46f50aabd913f784ce
nyangoto <patricknyangoto@hotmail.com>
2021-06-04 08:55:05 +0000

    Code refactor

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,7 @@ def read_file(filename=""):
     """
     Reads text file and prints to STDOUT
     """
-    with open(filename, "r", encoding='utf-8') as a_file:
-        print("{}".format(a_file.read(), end="")
+
+    with open(filename, encoding="UTF-8") as f:
+        for line in f:
+            print(line, end="")

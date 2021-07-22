721162e64a2d37848672f4957d92b90fbfa6c8a7
nyangoto <patricknyangoto@hotmail.com>
2021-07-22 00:23:36 +0000

    Addded 101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Display the average temperature by city ordered by temperature (dec)
+-- from the 'temperatures' table in hbtn_0c_0 database
+SELECT city,
+AVG(value) as 'avg_temp'
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;

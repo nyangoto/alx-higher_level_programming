fb5f95502c0c04c7cc668f19fcf795e1dff6207b
nyangoto <patricknyangoto@hotmail.com>
2021-08-09 18:42:33 +0000

    Syntax correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -7,5 +7,5 @@ SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
 /* Grant new user all privileges on the MySQL Server */
 GRANT ALL PRIVILEGES
 	ON *.*
-	TO user_0d_1@localhost
+	TO user_0d_1@localhost;
 

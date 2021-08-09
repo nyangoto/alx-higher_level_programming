1211425efdce9f8e2ac985238bcbc0c2b8fda1cb
nyangoto <patricknyangoto@hotmail.com>
2021-08-09 18:55:22 +0000

    Stylish much

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- Lists all privileges of the MySQL users 'user_0d_1' and 'user_0d_2'
 -- on localhost server.
 SHOW GRANTS FOR user_0d_1@localhost;
+/* Show privs for user_0d_2 */
 SHOW GRANTS FOR user_0d_2@localhost;

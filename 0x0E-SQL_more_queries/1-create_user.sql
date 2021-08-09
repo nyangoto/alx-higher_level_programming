6f7c043a0983b5cd18f2b876d53c9464c53ace3a
nyangoto <patricknyangoto@hotmail.com>
2021-08-09 18:35:17 +0000

    Added 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,11 @@
+-- Create a MySQL server user 'user_0d_1' whose password is 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS user_0d_1@localhost;
+
+/* SET password for new user */
+SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
+
+/* Grant new user all privileges on the MySQL Server */
+GRANT ALL PRIVILEGES
+	ON *.*
+	TO user_0d_1@localhost
+

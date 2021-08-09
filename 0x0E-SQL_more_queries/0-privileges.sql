093de25c3fb9bb54aeb59c0b01a13548bb4c6d99
nyangoto <patricknyangoto@hotmail.com>
2021-08-09 19:20:02 +0000

    Quotes

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- Lists all privileges of the MySQL users 'user_0d_1' and 'user_0d_2'
 -- Show privs for user_0d_1
-SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- Show privs for user_0d_2
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 

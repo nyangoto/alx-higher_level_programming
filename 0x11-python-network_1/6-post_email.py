4e7b8a297e6b6c7c1cd223443196564cabf14cfe
nyangoto <patricknyangoto@hotmail.com>
2021-08-26 19:02:41 +0000

    Task 6 script

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email address, sends a
+POST request to the passed URL with the email as a parameter, and
+finally displays the body of the response.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    response = requests.post(argv[1], data={'email': argv[2]})
+    print(response.text)

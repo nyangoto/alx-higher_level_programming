bfab2a929f3f0f8f245f25e7af7e0d3771574826
nyangoto <patricknyangoto@hotmail.com>
2021-08-26 17:52:37 +0000

    Removed trailing whitespace on line 4

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Python script that takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter, and 
+request to the passed URL with the email as a parameter, and
 displays the body of the response (decoded in utf-8)
 """
 

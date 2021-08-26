9f1a1993ccec91f3ffc16a3eba8f163ed1945c7c
nyangoto <patricknyangoto@hotmail.com>
2021-08-26 17:48:34 +0000

    Added task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and 
+displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as r:
+        html = r.read()
+        print('{}'.format(html.decode('utf-8')))
